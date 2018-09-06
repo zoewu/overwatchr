f9d2d0997ee21d667e85534aa75528a63b68414e
Zoe Wu <zoewxy@gmail.com>
2018-09-05 23:50:36 -0700

    page commit

--- R/prep_api.R
+++ R/prep_api.R
@@ -1,5 +1,7 @@
 install.packages("tidyverse")
+install.packages("DT")
 library(jsonlite)
+library(DT)
 
 # refernce: https://www.reddit.com/r/Competitiveoverwatch/comments/8zmpyr/api_overwatch_league_is_anyone_creating_a_forum/
 ?jsonlite
@@ -7,4 +9,7 @@ root = "https://api.overwatchleague.com/stats"
 stat = "players"
 path = paste(root,stat,sep = "/")
 flatJSON <- fromJSON(path)
-
+data <- flatJSON$data
+datatable(data)
+data[-c(1,2)]
+stringi::stri_trans_totitle(gsub("_"," ",names(data)[-1:2]))
